b31dc1884f831fe78d7e54ad21e0522caa083d75
alegara99 <alegaranzini@gmail.com>
2024-03-30 21:48:55 +0100

    feat: added Link to home

--- frontend/src/components/ToolLogo.js
+++ frontend/src/components/ToolLogo.js
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function ToolLogo() {
   return (
     <div className="flex items-center justify-center">
+      <Link href="/">
       <Image className="mb-5" src="/logo.png" width={180} height={180} />
+      </Link>
       <p className="text-white font-bold mb-3 mt-3">
         INTER <br /> TEAM MANAGER
       </p>
