39bfc1808ed3c556b8a2a79c813a7e54172b829a
alegara99 <alegaranzini@gmail.com>
2024-04-01 19:10:51 +0200

    feat: now the table is a little bit more responsive

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -50,7 +50,7 @@ export default function Members() {
       <ToolLogo />
       <h2 className="text-2xl text-white font-semibold mb-6">TEAM DASHBOARD</h2>
 
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4 overflow-x-auto">
         <div className="flex flex-col items-end justify-end mb-5">
           <Link
             href={`/members/add`}
@@ -59,59 +59,61 @@ export default function Members() {
             AGGIUNGI TEAM MEMBER
           </Link>
         </div>
-        <table className="min-w-full rounded-lg table-auto shadow-2xl">
-          <thead>
-            <tr>
-              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                Nome
-              </th>
-              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                Email
-              </th>
-              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                Data di Nascita
-              </th>
-              <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                Ruolo
-              </th>
-              <th className=" py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                Azioni
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {members.map((member) => (
-              <tr key={member.email}>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  {member.name}
-                </td>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  {member.email}
-                </td>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  {member.birthdate}
-                </td>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  {member.role}
-                </td>
-                <td className="py-5 border-b border-gray-200 bg-white text-sm">
-                  <Link
-                    href={`/members/edit/${member.id}`}
-                    className="bg-blue-900 hover:bg-black text-white  font-bold py-2 px-4 transition duration-300"
-                  >
-                    MODIFICA
-                  </Link>
-                  <button
-                    onClick={() => deleteMember(member.id)}
-                    className="bg-red-700 hover:bg-black text-white font-bold ml-4 py-2 px-4 transition duration-300 cursor-pointer"
-                  >
-                    ELIMINA
-                  </button>
-                </td>
+        <div className="min-w-full lg:min-w-0">
+          <table className="min-w-full rounded-lg table-auto shadow-2xl">
+            <thead>
+              <tr>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Nome
+                </th>
+                <th className="hidden md:table-cell px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Email
+                </th>
+                <th className="hidden md:table-cell px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Data di Nascita
+                </th>
+                <th className="hidden md:table-cell px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Ruolo
+                </th>
+                <th className=" py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Azioni
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {members.map((member) => (
+                <tr key={member.email}>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {member.name}
+                  </td>
+                  <td className="hidden md:table-cell px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {member.email}
+                  </td>
+                  <td className="hidden md:table-cell px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {member.birthdate}
+                  </td>
+                  <td className="hidden md:table-cell px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    {member.role}
+                  </td>
+                  <td className="py-5 border-b border-gray-200 bg-white text-sm flex flex-col sm:flex-row gap-2 sm:gap-0">
+                    <Link
+                      href={`/members/edit/${member.id}`}
+                      className="bg-blue-900 hover:bg-black text-white text-center font-bold py-2 px-4 transition duration-300 cursor-pointer sm:ml-4"
+                    >
+                      MODIFICA
+                    </Link>
+                    <button
+                      onClick={() => deleteMember(member.id)}
+                      className="bg-red-700 hover:bg-black text-white font-bold py-2 px-4 transition duration-300 cursor-pointer sm:ml-4"
+                    >
+                      ELIMINA
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <div className="flex justify-between items-center mt-4">
           <button
             onClick={() => setCurrentPage(currentPage - 1)}
